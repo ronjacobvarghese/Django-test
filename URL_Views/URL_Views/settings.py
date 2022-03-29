a6e353879a633d888dfff76b9341c447e3831b8b
Ron Jacob <generalsmoke23@gmail.com>
2022-03-29 20:22:12 +0530

    test-app-1 done

--- URL_Views/URL_Views/settings.py
+++ URL_Views/URL_Views/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenges',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,10 @@ ROOT_URLCONF = 'URL_Views.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # BASE_DIR / "challenges" / "templates"
+            BASE_DIR/ "templates"
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR/ "static",
+]
--- URL_Views/challenges/views.py
+++ URL_Views/challenges/views.py
@@ -1,49 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 monthly_challenges = {
-  "january": "This month is january",
-  "febuary": "This month is febuary",
-  "march": "This month is march",
-  "july": "This month is july",
-  "june": " This month is june",
-  "august": "This month is august",
-  "april": "This month is april",
-  "may": "This month is may",
-  "september":"This month is september",
-  "october":"This month is october",
-  "november": "This month is november",
-  "december": "This month is december",
+    "january": "This month is january",
+    "febuary": "This month is febuary",
+    "march": "This month is march",
+    "july": "This month is july",
+    "june": " This month is june",
+    "august": "This month is august",
+    "april": "This month is april",
+    "may": "This month is may",
+    "september": "This month is september",
+    "october": "This month is october",
+    "november": "This month is november",
+    "december": None,
 }
 
+
 def index(request):
-  list_items = ""
-  months = list(monthly_challenges.keys())
-  
-  for month in months:
-    capitalize_month = month.capitalize()
-    month_path  = reverse("monthly-challenge", args = [month])
-    list_items += f"<li><a href = \"{month_path}\">{capitalize_month}</a></li>"
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
-  response_data= f"<ul>{list_items}</ul>"
-  return HttpResponse(response_data)
 
-def monthly_number(request,month):
-  months = list(monthly_challenges.keys())
-  
-  if month > len(months):
-    return HttpResponseNotFound("Invalid Month!!")
-  redirect_month = months[month-1]
-  redirect_path = reverse("monthly-challenge",args = [ redirect_month ])
-  return  HttpResponseRedirect(redirect_path)
+def monthly_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month!!")
+
+    redirect_month = months[month-1]
+    redirect_path = reverse("monthly-challenge", args=[redirect_month])
+
+    return HttpResponseRedirect(redirect_path)
+
 
 def monthly_challenge(request, month):
-  try:
-    challenge_text = monthly_challenges[month]
-    response_data = f"<h1>{challenge_text}</h1>"
-    return HttpResponse(response_data)
-  except:
-    return HttpResponseNotFound("<h1> This month is not supported</h1>")
-   
-  
+    try:
+        challenge_text = monthly_challenges[month]
+
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "header": month,
+        })
+
+    except:
+        raise Http404()
